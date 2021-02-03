6fac2d622679a93421ee38641a9b73c2b7616a20
Roman Kuite <roman.kuite@gmail.com>
2021-02-03 14:36:00 +0100

    Added getProductList Test

--- src/test/java/de/neuefische/studentdb/service/ProductServiceTest.java
+++ src/test/java/de/neuefische/studentdb/service/ProductServiceTest.java
@@ -1,14 +1,41 @@
 package de.neuefische.studentdb.service;
 
+import de.neuefische.studentdb.database.ProductDb;
+import de.neuefische.studentdb.model.Product;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProductServiceTest {
 
     @Test
     void getProductListTest() {
 
+        // Given
+        Product firstProduct = new Product("12","Shampoo");
+        Product secondProduct = new Product("13","Conditioner");
+
+        ProductDb mockedDB = mock(ProductDb.class);
+        when(mockedDB.getProductList()).thenReturn(List.of(
+                firstProduct,
+                secondProduct
+        ));
+
+        ProductService productServiceTest = new ProductService(mockedDB);
+        List<Product> actual = productServiceTest.getProductList("Sh");
+        // When
+        List<Product> expected = new ArrayList<>();
+        expected.add(firstProduct);
+        // Then
+        assertThat(actual, containsInAnyOrder(firstProduct));
     }
 
 }
